775dbf0198e06583392d9469a29cbc7fbabdc304
PH Sousa <91646825+phsousa0@users.noreply.github.com>
2021-10-22 10:20:01 -0400

    Update and rename vos-pack to vos-pack.sh

---
+++ vos-pack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script: Viena OS Pack 
+# Autor: PH Sousa <eng.phs@outllok.com>
+# Versão: 1.0
+
+echo "Executa instalação de codecs, plugins e utilitários para automação da distribuição Viena OS"
+
+sudo apt update && sudo apt install samba
