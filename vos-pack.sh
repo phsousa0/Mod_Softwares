c1029fe443f94029e50cdd6766f1d99a5c442d6f
PH Sousa <91646825+phsousa0@users.noreply.github.com>
2021-10-22 15:36:07 -0400

    Update vos-pack.sh

--- vos-pack.sh
+++ vos-pack.sh
@@ -6,4 +6,8 @@
 
 echo "Executa instalação de codecs, plugins e utilitários para automação da distribuição Viena OS"
 
-sudo apt update && sudo apt install samba
+sudo apt update && sudo apt install samba sox default-jre cdrdao cue2toc p7zip-rar rar rarcrack unrar-free ubuntu-restricted-extras ubuntu-restricted-addons xubuntu-restricted-addons brother-cups-wrapper-ac brother-cups-wrapper-bh7 brother-cups-wrapper-common brother-cups-wrapper-extra brother-cups-wrapper-laser brother-cups-wrapper-laser1 brother-cups-wrapper-mfc9420cn brother-lpr-drivers-ac brother-lpr-drivers-bh7 brother-lpr-drivers-common brother-lpr-drivers-extra brother-lpr-drivers-laser brother-lpr-drivers-laser1 brother-lpr-drivers-mfc9420cn printer-driver-all printer-driver-cups-pdf printer-driver-indexbraille printer-driver-oki smbc smbclient smbmap libnss-winbind libpam-winbind winbind -y
+
+echo "Prontinho, tudo instalado com sucesso!"
+
+exit
